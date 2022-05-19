821fb7734e61134b8d343e105b187784e863c2e0
Alex Nguyen <nghi@brandsvietnam.com>
2022-05-19 20:42:35 +0700

    No more authors to load

--- packages/algolia-netlify/functions/post-published.js
+++ packages/algolia-netlify/functions/post-published.js
@@ -1,7 +1,7 @@
 const IndexFactory = require('@tryghost/algolia-indexer');
 const transforms = require('@tryghost/algolia-fragmenter');
 
-// some content change ĐM
+// vậy là không còn mảng authors
 exports.handler = async (event) => {
     if (process.env.ALGOLIA_ACTIVE !== 'TRUE') {
         return {
