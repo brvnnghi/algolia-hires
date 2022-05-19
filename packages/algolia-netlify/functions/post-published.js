41f35431d1b4cb1044d11a798bee92facf714df9
Alex Nguyen <nghi@brandsvietnam.com>
2022-05-19 20:05:59 +0700

    Update post-published.js

--- packages/algolia-netlify/functions/post-published.js
+++ packages/algolia-netlify/functions/post-published.js
@@ -25,8 +25,6 @@ exports.handler = async (event) => {
         };
     }
 
-    post.authors.push({name: 'Alex', slug: 'Nghi'});
-
     const node = [];
 
     // Transformer methods need an Array of Objects
@@ -44,7 +42,7 @@ exports.handler = async (event) => {
         const index = new IndexFactory(algoliaSettings);
         await index.setSettingsForIndex();
         await index.save(fragments);
-        console.log(`Fragments "${post.authors}" successfully saved to Algolia index`); // eslint-disable-line no-console
+        console.log(`Fragments "${post.tags}" successfully saved to Algolia index`); // eslint-disable-line no-console
         return {
             statusCode: 200,
             body: `Post "${post.title}" has been added to the index.`
